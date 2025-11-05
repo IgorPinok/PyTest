ca0f06ca35da8031e69fa3c40db2dab21e59de79
Pinok <igorpinok@gmail.com>
2025-11-05 06:07:03 +0100

    test2

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,8 @@
-from TEST.main import A
+from TEST.main import Calculator
 
-def test_main():
-    assert A.x == 1
-
-def test_2():
-    assert 2 == 2
+def test_divide():
+    x = 1
+    y = 2
+    res = 0.5
+    assert Calculator().divide(x, y) == res
+ 
